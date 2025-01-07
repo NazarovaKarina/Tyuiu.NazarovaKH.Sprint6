67dc35ffb363b6c0d61aa7271440744028f840cb
Tyuiu.NazarovaKKH@outlook.com <Tyuiu.NazarovaKKH@outlook.com>
2025-01-07 17:26:31 +0300

    Task7

--- Tyuiu.NazarovaKH.Sprint6.Task7.V5/FormAbout.Designer.cs
+++ Tyuiu.NazarovaKH.Sprint6.Task7.V5/FormAbout.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAbout));
             buttonDone_NKH = new Button();
             pictureBoxOne_NKH = new PictureBox();
             textBoxInfo_NKH = new TextBox();
@@ -37,9 +36,10 @@
             // 
             // buttonDone_NKH
             // 
-            buttonDone_NKH.Location = new Point(477, 187);
+            buttonDone_NKH.Location = new Point(545, 249);
+            buttonDone_NKH.Margin = new Padding(3, 4, 3, 4);
             buttonDone_NKH.Name = "buttonDone_NKH";
-            buttonDone_NKH.Size = new Size(75, 23);
+            buttonDone_NKH.Size = new Size(86, 31);
             buttonDone_NKH.TabIndex = 5;
             buttonDone_NKH.Text = "ОК";
             buttonDone_NKH.UseVisualStyleBackColor = true;
@@ -47,32 +47,35 @@
             // pictureBoxOne_NKH
             // 
             pictureBoxOne_NKH.AccessibleRole = AccessibleRole.IpAddress;
-            pictureBoxOne_NKH.Location = new Point(12, 12);
+            pictureBoxOne_NKH.Location = new Point(14, 16);
+            pictureBoxOne_NKH.Margin = new Padding(3, 4, 3, 4);
             pictureBoxOne_NKH.Name = "pictureBoxOne_NKH";
-            pictureBoxOne_NKH.Size = new Size(166, 198);
+            pictureBoxOne_NKH.Size = new Size(190, 264);
             pictureBoxOne_NKH.TabIndex = 4;
             pictureBoxOne_NKH.TabStop = false;
             // 
             // textBoxInfo_NKH
             // 
             textBoxInfo_NKH.BorderStyle = BorderStyle.None;
-            textBoxInfo_NKH.Location = new Point(184, 12);
+            textBoxInfo_NKH.Location = new Point(210, 16);
+            textBoxInfo_NKH.Margin = new Padding(3, 4, 3, 4);
             textBoxInfo_NKH.Multiline = true;
             textBoxInfo_NKH.Name = "textBoxInfo_NKH";
             textBoxInfo_NKH.ReadOnly = true;
-            textBoxInfo_NKH.Size = new Size(368, 161);
+            textBoxInfo_NKH.Size = new Size(421, 215);
             textBoxInfo_NKH.TabIndex = 3;
-            textBoxInfo_NKH.Text = resources.GetString("textBoxInfo_SNM.Text");
+            textBoxInfo_NKH.Text = "Разработчик:\r\nНазарова Карина Халиловна\r\n\r\nГруппа:\r\nИСПб-24-1\r\n\r\nСпринт 6 \r\nЗадание 7 \r\nВариант 5";
             // 
             // FormAbout
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(584, 261);
+            ClientSize = new Size(667, 348);
             Controls.Add(buttonDone_NKH);
             Controls.Add(pictureBoxOne_NKH);
             Controls.Add(textBoxInfo_NKH);
-            MinimumSize = new Size(600, 300);
+            Margin = new Padding(3, 4, 3, 4);
+            MinimumSize = new Size(683, 384);
             Name = "FormAbout";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "О программе";
